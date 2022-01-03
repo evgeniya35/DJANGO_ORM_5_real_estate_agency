0fe50c152f134750f5ec2c7c90c7cedd029fd095
evgeniya35 <evgeniya35@yandex.ru>
2022-01-03 11:06:43 +0300

    Customize Flat Admin

--- property/admin.py
+++ property/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from .models import Flat
 
 class FlatAdmin(admin.ModelAdmin):
-    search_fields = ('town', 'address')
+    search_fields = ('town', 'address',)
+    readonly_fields = ('created_at',)
 
 admin.site.register(Flat, FlatAdmin)
