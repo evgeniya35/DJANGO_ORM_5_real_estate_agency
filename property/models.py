9d1187f82b621d2bd78aad6ebdb90009fa6b2e18
evgeniya35 <evgeniya35@yandex.ru>
2022-01-03 11:33:30 +0300

    Add model field Flat.new_building

--- property/models.py
+++ property/models.py
@@ -46,6 +46,13 @@ class Flat(models.Model):
         null=True,
         blank=True,
         db_index=True)
+    
+    NEW_BUILDINGS_CHOICES = ((True, 'новостройка'), (False, 'старое здание'), (None, 'не заполнено'))
+    new_building = models.NullBooleanField(
+        'Новостройка',
+        choices=NEW_BUILDINGS_CHOICES,
+        default=None,
+        db_index=True,)
 
     def __str__(self):
         return f'{self.town}, {self.address} ({self.price}р.)'
